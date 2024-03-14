b84329e809cb8e34784e950b425f719bcff785fb
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-03-14 11:19:19 +0000

    conversion of datatypes

---
+++ datatpes/conversionofdatypes.js
@@ -0,0 +1,45 @@
+/*
+//let score=33;
+// let score=null;
+ let score=datatype;
+console.log(typeof score);         //using any ones
+console.log(typeof (score));
+
+
+let score1="33abc";         //score1 is a String type variable
+console.log(typeof score1);
+let ValueofNumber= Number(score1);
+console.log(typeof ValueofNumber);           //hence it convert a string value to number
+console.log(typeof ValueofNumber);
+
+console.log(ValueofNumber);             //o/p NaN means it is js debug/javascript o/p is not a wrong
+
+// datatype     typeof
+//"33" ==>33 String
+// true===> 1 Boolean
+//"33abc"===> NaN  number
+// null========>object
+// datatype===> number
+ 
+*/
+/*
+//BOOLEAN CONVERSION
+
+let isLoggedin = 1;        // 1==> true  "vishal"=>true
+//let isLoggedin= ""      // emptyvalue o/p false
+let booleanisLoggedin=Boolean(isLoggedin);
+
+let isLoggedin1=Boolean(isLoggedin);
+console.log(typeof booleanisLoggedin);
+
+console.log(booleanisLoggedin);
+
+*/
+
+//STRING CONVERSION
+
+let value=33;
+
+let StringValueConvert=String(value);    //Conever to String
+console.log(typeof StringValueConvert)  //Converted to String
+console.log(StringValueConvert);
