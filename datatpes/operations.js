c393e0745ba31b31aebe577500f07d376a35cbee
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-03-14 11:48:48 +0000

    prefix & postfix operation

---
+++ datatpes/operations.js
@@ -0,0 +1,21 @@
+/*
+let value=3;
+let valueNagetive=-value;
+console.log(valueNagetive);
+*/
+
+//****************PREFIX & POSTFIX *******************************
+
+/* In the prefix version (++i), the value of i is incremented first, and the value of the expression is the new value of i.
+In the postfix version (i++), the value of i is incremented, but the value of the expression is the original value of i.
+*/
+//PREFIC//
+let a=10;
+let b= ++a;          //increase by one
+console.log(b);
+
+//POSTFIX//
+
+let c=20;
+let d=c++;          //not incresed
+console.log(d);
