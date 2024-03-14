14e08e2acbfe2a93187efc9b216f61b26a882363
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-03-14 10:43:35 +0000

    datatypes 1st class

---
+++ dattype.js
@@ -0,0 +1,12 @@
+let name="vishal"
+// number
+// bigint
+// String
+// Boolean
+// null  =======>o/p object
+// undefined =====>o/p undefined
+//symobol
+
+console.log(typeof "vishal");
+console.log(typeof null);         //object
+console.log(typeof undefined);
