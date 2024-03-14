112d3e2833d3a0959e1a8e74fd4e1555ee826eee
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-03-14 10:21:53 +0000

    variable comleted

--- variable.js
+++ variable.js
@@ -1,4 +1,17 @@
 let a=3;
 let b=10;
-
+const accountPassword=1234;
 console.log(a+b);
+
+let accountname="vishal"
+var accountcity="pune"
+let accountstate;
+
+console.log(accountPassword)
+console.log(accountname);
+console.log(accountcity);
+
+console.table([accountPassword,accountcity,accountname,accountstate]); //in tabular form
+ b=4;
+console.log(b);
+
