e2865042cfdccb309f372339ba689872a10e692d
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-03-17 08:14:41 +0000

    Date and Time

---
+++ datatpes/dates.js
@@ -0,0 +1,14 @@
+////date/////  MONTH START WITH O ZERO
+/*
+let myDate=new Date()
+console.log(myDate);
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(myDate.getMonth())
+
+console.log(typeof myDate)        //objecct its imp
+
+*/
+
+let toDate=new Date(2024,0,23);    //MONTH START WITH 0
+console.log(toDate.toDateString());
