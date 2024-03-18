00633ae649b6690528e6d4c0d676b71741f066f5
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-03-18 00:44:08 +0000

    date & time

--- datatpes/dates.js
+++ datatpes/dates.js
@@ -12,3 +12,9 @@ console.log(typeof myDate)        //objecct its imp
 
 let toDate=new Date(2024,0,23);    //MONTH START WITH 0
 console.log(toDate.toDateString());
+
+//console.log(toDate.floor(toDate.now()+1000));
+
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
