14220d06ed612d3dc1270393e189d59160d18219
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-05-24 13:50:39 +0000

    variable2 practice

---
+++ variable2.js
@@ -0,0 +1,11 @@
+const accountname="vishal";
+
+let name="deshmukh";
+let name1="ram";
+//const accountname="ram";
+
+console.log(name)
+console.log(accountname)
+console.log(name1)
+
+console.log([name,accountname,name1]);
