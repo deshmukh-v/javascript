a145c7a1c952c22d375d2680a0d4f2719d494a05
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-05-29 03:17:57 +0000

    value converstion

---
+++ converstionpractice.js
@@ -0,0 +1,23 @@
+let value=123
+
+console.log(value)
+console.log(typeof value);             // typeof value is Number
+
+
+// value convert Number to String
+
+let StringNumber = String(value)
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
+//
+// value is String Convert To Boolean
+let Number ="true"
+
+console.log(typeof Number)               // o/p ==> true (i.e String)
+
+let BooleanNumber=Boolean(Number)       // convert String to Boolean 
+console.log(BooleanNumber);              // o/p ===>true
+console.log(typeof BooleanNumber)        // o/p ===>boolean value (convert String to boolean)
+
+
