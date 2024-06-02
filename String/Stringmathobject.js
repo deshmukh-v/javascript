9797e554bb5e2025e829a0fe2e5c4ac29848382b
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-06-02 05:29:26 +0000

    StringMathObject

---
+++ String/Stringmathobject.js
@@ -0,0 +1,14 @@
+
+// Math.round() round a number to the nearest integer
+let age=10.77
+console.log(Math.round(age));
+
+console.log(Math.floor(age));     // Math.floor() floor a number down to the nearest Interger value
+
+
+// converting String to Number value
+ 
+let number="20"
+let No=Number(number)
+console.log(No)
+console.log(typeof No)
