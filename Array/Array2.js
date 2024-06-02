c5a0f1e65f59fd73a0b969d90cabb404ec3d0a6f
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-06-02 17:25:32 +0000

    Array 2

---
+++ Array/Array2.js
@@ -0,0 +1,36 @@
+/*
+const Super_heros=["superman","BadMan","Shaktiman"]
+
+const Marvel_Heroes=["Thor","Spiderman","Iron man"]
+
+// console.log(Super_heros)
+// console.log(Marvel_Heroes)
+
+const All_Heroes=[...Marvel_Heroes,...Super_heros]        // ALL ARRAYS ARE CONCANITET i.e. SPREAD VALUE ARRAY
+console.log(All_Heroes)
+
+*/
+
+/*
+
+let GroupofArray=[1,2,3,[4,5],6,7,8,[9,10,11,[12,13,14]]]
+// console.log(GroupofArray);
+
+let CountallArraay=GroupofArray.flat(Infinity)           //using flat() cancniate all array
+console.log(CountallArraay);
+
+*/
+
+// console.log(Array.isArray("vishal"))
+// console.log(Array.from("vishaldeshmukh"))               // convert array
+   console.log(Array.from({Name:"vishal"}))
+
+
+
+let score1=100
+let score2=200
+let score3=300
+console.log(typeof score1)
+console.log(Array.of(score1,score2,score3))
+console.log(typeof Array)
+
