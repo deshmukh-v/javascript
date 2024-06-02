b2783ce996a5f064d35e79bdc43a520f9e530733
deshmukh-v <154399074+deshmukh-v@users.noreply.github.com>
2024-06-02 16:04:15 +0000

    date & timezon

---
+++ date.js
@@ -0,0 +1,35 @@
+let Mydate = new Date();
+console.log(typeof Mydate);
+
+console.log(Mydate)
+ //console.log(toString.Mydate)
+ //console.log(Mydate.toString())
+ //console.log(Mydate.toDateString());          //it is mostly used
+ console.log(Mydate.toLocaleString());        //it is mostly used
+
+/*
+// let Newdate=new Date("1-14-2014");           //indian standard Month/date/year
+
+// console.log(Newdate.toLocaleString())
+
+
+
+// console.log(Newdate.getTime());             //time in milisecond
+/*
+let MyTimeStamp= Date.now()
+console.log(MyTimeStamp);
+
+console.log(Date.now()/1000);                  //convert to second
+
+console.log(Math.floor(Date.now()/1000))        // decimal point are cut
+
+*/
+/*
+let Timex=Date.now()
+
+console.log(Timex);
+console.log(Date.now())
+console.log(Date.now()/1000);                // convert to second (milisecond convert to second)
+console.log(Math.floor(Date.now()/1000));
+
+*/
